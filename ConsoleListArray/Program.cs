a1c2a34184a991bd47227c6e1f6bcd4259b1e42c
Anastasiia <vostrova_1994@mail.ru>
2022-03-22 16:19:06 +0300

    доделала задания. осталась оптимизация и тесты

--- ConsoleListArray/Program.cs
+++ ConsoleListArray/Program.cs
@@ -3,10 +3,20 @@
 
 
 
-ClassListArray list = new ClassListArray();
-//for (int i=0;i<10;i++)
+ClassListArray list = new ClassListArray(new int[] {5,9,4,9,3,4});
+list.Write();
+int x = list.ReturnAmountDeleted(4);
+list.Write();
+Console.WriteLine(x);
 
-//list.Add(2);
+
+
+//list.Add(4);
+//for (int i = 0; i < 5; i++)
+//{
+//    list.Add(i);
+//}
+
 //list.Write();
 //list.AddInStart(2);
 //list.Write();
@@ -19,13 +29,31 @@ ClassListArray list = new ClassListArray();
 //list.Write();
 //list.DeletIndex(2,2);
 //list.Write();
-
-
-
-
-
-//list.DeletIndex(2);
+//list.DelenNEnd(3);
 //list.Write();
+//list.DeletNStart(0);
+////list.Write();
+//list.DeletNIndex(0,5);
+//list.Write();
+//list.AccessForIndex(5);
+//list.Write();
+//list.ShowIndex(1);
+//list.Write();
+//list.ChangeByIndex(1,8);
+//list.Write();
+//list.ReverseArray();
+//list.Write();
+//list.SearchMax();
+//list.Write();
+//list.SearchIndexMax();
+//list.Write();
+//list.SortArrayByAge();
+//list.Write();
+//list.SortArrayDescending();
+//list.Write();
+
+
+
 
 
 
--- MyFirstProjectListArray/ClassListArray.cs
+++ MyFirstProjectListArray/ClassListArray.cs
@@ -8,8 +8,29 @@
 
         public ClassListArray()
         {
+            _array = new int[10];        
+            Length = 0;
+        }
+        public ClassListArray(int element)
+        {
             _array = new int[10];
-            Length = 6;
+            _array = new int[element];           
+            Length = 0;
+        }
+
+        public ClassListArray(int[] array)
+        {
+            if (array == null || array.Length == 0)
+            {
+                _array = new int[10];
+                Length = 0;
+            }
+            else
+            {
+                _array = array;
+                Length = array.Length;
+                UpSize();
+            }
         }
 
         public void Add(int value)
@@ -48,23 +69,22 @@
             Length++;
         }
 
-        public void DeletLast(int value)
+        public void DeletLast()
         {
             if (Length>= _array.Length)
             {
                 DeletValue();
-            }
-            _array[0] = value;
+            }          
             Length--;
         }
 
-        public void DeletStart()
+        public void DeletStart(int value)
         {
             if (Length >= _array.Length)
             {
                 DeletValue();
             }
-            
+            _array[0] = value;
             Length--;
         }
 
@@ -75,6 +95,216 @@
             }
             _array[index] = value;
             Length--;
+        }
+
+        public void DeletNEnd(int value)
+        {
+            Length = Length - value;
+            int[] newArray=new int[Length];
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                newArray[i] = _array[i];
+            }
+            _array= newArray;
+
+        }
+
+        public void DeletNStart(int value)
+        {
+            Length = Length - value;
+            int[] newArray = new int[Length];
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                newArray[i] = _array[i + value];
+            }
+            _array = newArray;
+        }
+
+        public void DeletNIndex(int index,int value)
+        {
+            Length = Length - value;
+            int[] newArray = new int[Length];
+
+            for (int i = index; i < index; i++)
+                newArray[i] = _array[i];
+
+            for (int i = index; i < newArray.Length; i++)
+                newArray[i] = _array[i+value];
+
+            _array = newArray;
+        }
+
+        public int ReturnRecovery(int value)
+        {
+            return Length;
+        }
+      
+        public int AccessForIndex (int index)
+        {
+            return _array[index];
+        }
+
+        public int ShowIndex(int value)
+        {
+            int index = -1;
+            for (int i = 0; i < Length; i++)
+            {
+                if (_array[i] == value)
+                {
+                    
+                    return i;
+                    
+                }
+            }
+            return -1;
+        }
+
+        public void ChangeByIndex(int index,int value)
+        {
+            _array[index] = value;
+        }
+
+        public void ReverseArray ()
+        {         
+            for (int i = 0; i < Length / 2; i++)
+            {
+                int tmp = _array[i];
+                _array[i] =_array[Length - (1 + i)];
+                _array[Length-(1 + i)] = tmp;
+            }            
+        }
+
+        public int SearchMax()
+        {
+            int maxValue;
+            maxValue = _array[0];
+            for (int i = 1; i < Length; i++)
+            {
+
+                if (_array[i] > maxValue)
+                {
+                    maxValue = _array[i];
+                }
+            }
+            return maxValue;
+        }
+
+        public int SearchMin()
+        {
+            int minValue;
+            minValue = _array[0];
+
+            for (int i = 1; i < Length; i++)
+            {
+                if (_array[i] < minValue)
+                {
+                    minValue = _array[i];
+                    
+                }
+
+            }
+            return minValue;
+        }
+
+        public int SearchIndexMax()
+        {
+            int maxValue = _array[0];
+            int index = 0;
+            for (int i = 1; i < Length; i++)
+            {
+                if (_array[i] > maxValue)
+                {
+                    maxValue = _array[i];
+                    index = i;
+                }
+
+            }
+            return index;
+        }
+
+        public int SearchIndexMin()
+        {
+            int minValue = _array[0];
+            int index = 0;
+
+            for (int i = 1; i < Length; i++)
+            {
+                if (_array[i] < minValue)
+                {
+                    minValue = _array[i];
+                    index = i;
+                }
+
+            }
+            return index;
+        }
+
+        public void SortArrayByAge()
+        {
+            for (int i = 0; i < Length; i += 1)
+            {
+                for (int j = 1 + i; j < Length; j++)
+                    if (_array[j] < _array[i])
+                    {
+                        int c = _array[i];
+                        _array[i] = _array[j];
+                        _array[j] = c;
+                    }
+            }
+        }
+
+        public void SortArrayDescending()
+        {
+            for (int i = 1; i < Length; i++)
+            {
+
+                int crnt = i;
+                while (crnt != 0 && _array[crnt - 1] < _array[crnt])
+                {
+                    int tmp = _array[crnt];
+                    _array[crnt] = _array[crnt - 1];
+                    _array[crnt - 1] = tmp;
+                    crnt--;
+                }
+            }
+        }
+
+        public int DeleteValueFirst(int value)
+        {
+            int count = 0;
+            for (int i = 0; i < Length; i++)
+            {
+                if (_array[i] == value)
+                {
+                    count++;
+                }
+                else
+                {
+                    _array[i - count] = _array[i];
+                }
+            }
+            Length -= count;
+
+            return count;
+        }
+
+        public int ReturnAmountDeleted(int value)
+        {
+           
+            int sum = 0;
+            for (int i = 1; i < Length; i ++)
+            {
+                if (_array[i] == value)
+                {
+                    sum++;
+                }
+                else
+                {
+                    _array[i - sum] = _array[i];
+                }
+            }
+            Length = Length-sum;
+            return sum;
         }
 
         public void Write()
